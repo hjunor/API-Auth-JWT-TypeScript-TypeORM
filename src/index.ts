3cd305e09cd51974bbb442d2ac4456e8e1034120
hjunor <hjunor@gmail.com>
2020-07-08 23:24:44 -0300

    add migration users

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import "reflect-metadata";
 import express from 'express';
 import routes from './routes'
-
+import './database/connect';
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('🔥🔥🔥🔥🔥'));
+app.listen(3333, () => console.log('🔥 Server start '));
